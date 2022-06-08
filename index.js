c518304072eb3ede095c1aeea3f2f3f68252056a
wachira <wachiraa954@gmail.com>
2022-06-08 16:10:55 +0300

    commit tree 37b57cdfc0fbc9e4870048693764dd03913f5a5a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Alex Wachira";
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}! `;
